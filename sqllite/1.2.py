ec2732ba2a1b6672670b9f70f72fe007ce068da9
Meresnes <meresnes@yandex.ru>
2020-05-10 22:35:51 +0300

    db_c1.21

--- sqllite/1.2.py
+++ sqllite/1.2.py
@@ -32,35 +32,48 @@ def reg():
 
     '''for value in sql.execute("SELECT * FROM users"):
         print(value)'''
+def chek_pass(user_login,user_password):
+    sql.execute(f"SELECT password FROM users WHERE login = '{user_login}'")
+    if user_password in sql.fetchone():
+        return True
+    else:
+        print('ПАРОЛЬ НЕ ВЕРЕН !!!!!!!!!!')
+        return False
 def casino():
     
     user_login = input('Log in:')
-    number = randint(1,1)
-
-
+    
     sql.execute(f"SElECT login FROM users WHERE login = '{user_login}'")
     if sql.fetchone() is None:
         print('Такого логина нет!')
         print('Зарегестрируйтесь!')
         reg()
     else:
-        #user_password = input('Введите пароль: ')
-        if number == 1:
-            sql.execute(f"UPDATE users SET cash = '{1000}' WHERE login = '{user_login}'")
-            db.commit()
-            
-            print('Ура победа!')
-            
-        else:
-            print('Вы проиграли !')
-
-        enter(user_login)
+        user_password = input('Введите пароль: ')
+        key = chek_pass(user_login,user_password)
+        if key == True:
+            number = randint(1,5)
+            if number == 1:
+                sql.execute(f"UPDATE users SET cash = '{1000}' WHERE login = '{user_login}'")
+                db.commit()
+                
+                print('Ура победа!')
+                
+            else:
+                print('Вы проиграли !')
+        
 def enter(user_name):
     for i in sql.execute(f"SELECT login, cash FROM users WHERE login = '{user_name}' "):
         print(i)
 
 def main():
     casino()
+    
+    for value in sql.execute("SELECT * FROM users"):
+        print(value)
+    sql.close()
+    db.close()
 
 main()
+
     
