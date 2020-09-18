c59f3b72b650335f6a2bc4adc3b3939b76d68b01
meresnes <meresnes@yandex.ru>
2020-09-18 22:03:30 +0300

    django

--- django/Django_1/site_1/mainApp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    return render(request,'homePage.html')
-
-def contact(request):
-    return render(request,'basic.html',{'values':['Если у вас остались вопросы, то задавайте из по телефону','8-911-222-55-12']})
