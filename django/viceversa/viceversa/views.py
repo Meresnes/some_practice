21d4059a2b457f037fac04e2623da4397e8ee3cf
meresnes <meresnes@yandex.ru>
2020-09-19 15:36:30 +0300

    add_word_counter

--- django/viceversa/viceversa/views.py
+++ django/viceversa/viceversa/views.py
@@ -6,6 +6,17 @@ def home(request):
 
 def reverse(request):
     user_text = request.GET['usertext']
+    words_len = 0
+    k = 0
+    for i in user_text:
+        if i == ' ':
+            words_len += 1
+        k += 1
+    if words_len > 0:
+        words_len += 1
+    elif k > 0:
+        words_len = 1
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'user_text': user_text,
+    return render(request, 'reverse.html', {'words': words_len,
+                                            'user_text': user_text,
                                            'reversed_text': reversed_text})
